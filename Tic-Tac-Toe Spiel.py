a06e60e4ec0fd25da0dcc50ad40a0d13dc292380
Jonas <jonas-zinne@web.de>
2024-08-28 18:57:59 +0200

    code improvement

--- Tic-Tac-Toe Spiel.py
+++ Tic-Tac-Toe Spiel.py
@@ -3,47 +3,41 @@ print("Tic-Tac-Toe Spiel")
 
 # Variablen
 spiel_aktiv = True
-feld_belegt = False
 spieler_aktuell = "X"
 
 # Spielfeld definieren
-spielfeld = ['', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
+spielfeld = [''] + [str(i) for i in range(1, 10)] 
 
 # Spielfeld ausgeben
 def spielfeld_ausgeben():
-    print(spielfeld[1] + '|' + spielfeld[2] + '|' + spielfeld[3])
-    print(spielfeld[4] + '|' + spielfeld[5] + '|' + spielfeld[6])
-    print(spielfeld[7] + '|' + spielfeld[8] + '|' + spielfeld[9])
+    for i in range(1, 10, 3):
+        print(f"{spielfeld[i]}|{spielfeld[i+1]}|{spielfeld[i+2]}")
 
 # Spielereingabe 
 def spieler_eingabe():
-    global spiel_aktiv, feld_belegt
+    global spiel_aktiv
 
     while True:
-        if feld_belegt:
-            spielzug = input("Bitte ein anderes Feld eingeben: ")
-        else:
-            spielzug = input("Bitte Feld eingeben: ")
+        spielzug = input("Eingabe Feld: ")
 
-        if spielzug == 'exit':
+        if spielzug.lower() == 'exit':
             spiel_aktiv = False
             print("Das Spiel wurde beendet.")
             return
 
         try:
             spielzug = int(spielzug)
-        except ValueError:
-            print("Bitte eine Zahl eingeben")
-        else:
             if 1 <= spielzug <= 9:
                 if spielfeld[spielzug] in ['X', 'O']:
-                    print("Dieses Feld ist bereits belegt!")
-                    feld_belegt = True
+                    print("Dieses Feld ist bereits belegt! Bitte ein anderes Feld eingeben.")
                 else:
                     return spielzug
             else:
-                print("Ungültige Eingabe. Bitte eine Zahl zwischen 1 und 9 eingeben")
+                print("Ungültige Eingabe. Bitte eine Zahl zwischen 1 und 9 eingeben.")
 
+        except ValueError:
+            print("Bitte eine Zahl eingeben.")
+            
 # Spieler wechseln
 def spieler_wechseln():
     global spieler_aktuell
@@ -52,18 +46,17 @@ def spieler_wechseln():
 
 # Kontrolle auf Gewinnen
 def kontrolle_gewonnen():
-    kombination = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-    for a, b, c in kombination:
+    kombinationen = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    for a, b, c in kombinationen:
         if spielfeld[a] == spielfeld[b] == spielfeld[c]:
             return spielfeld[a]
-    return None
+        return None
 
 # Kontrolle auf Unentschieden
 def kontrolle_unentschieden():
-    if (spielfeld[1] == 'X' or spielfeld[1] == 'O') and (spielfeld[2] == 'X' or spielfeld[2] == 'O') and (spielfeld[3] == 'X' or spielfeld[3] == 'O') and \
-       (spielfeld[4] == 'X' or spielfeld[4] == 'O') and (spielfeld[5] == 'X' or spielfeld[5] == 'O') and (spielfeld[6] == 'X' or spielfeld[6] == 'O') and \
-       (spielfeld[7] == 'X' or spielfeld[7] == 'O') and (spielfeld[8] == 'X' or spielfeld[8] == 'O') and (spielfeld[9] == 'X' or spielfeld[9] == 'O'):
-        return ('unentschieden')
+    if all(spielfeld[i] in ['X', 'O'] for i in range(1, 10)):
+        return 'unentschieden'
+    return None
 
 # Hauptschleife
 spielfeld_ausgeben()
